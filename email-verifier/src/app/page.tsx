60eb6cae039091ef1c625635c32d23a5a265f0bb
Zahidmohd <btech25082.21@bitmesra.ac.in>
2025-01-17 17:40:25 +0530

    2nd task

---
+++ email-verifier/src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EmailChecker from './components/EmailChecker'
+
+const page = () => {
+  return (
+    <div>
+      <EmailChecker/>
+    </div>
+  )
+}
+
+export default page
